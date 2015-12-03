76abd3beff65ad3025512bd3cb8fb0c1e2f4edc1
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-12-03 13:55:07 +0100

    Dynamic update

--- test/amigo.cpp
+++ test/amigo.cpp
@@ -23,35 +23,99 @@ void update(mwm::WorldModel& wm, const cv::Mat& depth_image, const geo::Pose3D& 
 
     int step = 10;
 
-    cv::Mat vertex_map(height / step, width / step, CV_32SC1, -1);
+    // - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    for(int y = 0; y < height; y += step)
+    cv::Mat vertex_map(height / step, width / step, CV_32SC1, cv::Scalar(-1));
+    cv::Mat vertex_map_z(height / step, width / step, CV_32FC1, 0.0);
+
+    geo::Pose3D sensor_pose_inv = sensor_pose.inverse();
+
+    const std::vector<geo::Vec3>& vertices = wm.vertices();
+    for(unsigned int i = 0; i < vertices.size(); ++i)
     {
-        for(int x = 0; x < width; x += step)
+        const geo::Vec3& p = vertices[i];
+
+        geo::Vec3 p_sensor = sensor_pose_inv * p;
+
+        float vz = -p_sensor.z;
+        if (vz < 0) // Filter vertices that are behind the camera
+            continue;
+
+        geo::Vec2i p_2d = P.project3Dto2D(p_sensor);
+        if (p_2d.x < 0 || p_2d.y < 0 || p_2d.x >= width || p_2d.y >= height)
+            continue;
+
+        int vx = p_2d.x / step;
+        int vy = p_2d.y / step;
+
+        int vi = vertex_map.at<int>(vy, vx);
+        if (vi <= 0 || vertex_map_z.at<float>(vy, vx) < vz)
         {
-            float d = depth_image.at<float>(y, x);
-            if (d != d || d == 0)
-                continue;
-
-            geo::Vec3 p = P.project2Dto3D(x, y) * d;
-
-            vertex_map.at<int>(y / step, x / step) = wm.addVertex(sensor_pose * p);
+            vertex_map.at<int>(p_2d.y / step, p_2d.x / step) = i;
+            vertex_map_z.at<float>(p_2d.y / step, p_2d.x / step) = -p_sensor.z;
         }
     }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    // Add missing vertices
+
+    cv::Mat vertex_map_new(height / step, width / step, CV_8UC1, cv::Scalar(0));
 
     for(int y = 0; y < vertex_map.rows - 1; ++y)
     {
         for(int x = 0; x < vertex_map.cols - 1; ++x)
         {
+            int ix = step * (x + 0.5);
+            int iy = step * (y + 0.5);
+
+            float d = depth_image.at<float>(iy, ix);
+            if (d != d || d <= 0.5 || d > 5)
+                continue;
+
+            int i = vertex_map.at<int>(y, x);
+            if (i >= 0 && std::abs(vertex_map_z.at<float>(y, x) - d) < 0.2)
+                continue;
+
+            geo::Vec3 p_sensor = P.project2Dto3D(ix, iy) * d;
+            geo::Vec3 p = sensor_pose * p_sensor;
+            vertex_map.at<int>(y, x) = wm.addVertex(p);
+            vertex_map_new.at<unsigned char>(y, x) = 1;
+        }
+    }
+
+//    cv::imshow("vertices", vertex_map_z / 10);
+//    cv::imshow("vertices_new", vertex_map_new * 255);
+//    cv::waitKey(3);
+
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    for(int y = 0; y < vertex_map.rows - 1; ++y)
+    {
+        for(int x = 0; x < vertex_map.cols - 1; ++x)
+        {
+//            int ix = (0.5 + step) * x;
+//            int iy = (0.5 + step) * y;
+
+//            float d = depth_image.at<float>(iy, ix);
+//            if (d != d || d == 0)
+//                continue;
+
             int i1 = vertex_map.at<int>(y, x);
             int i2 = vertex_map.at<int>(y, x + 1);
             int i3 = vertex_map.at<int>(y + 1, x);
             int i4 = vertex_map.at<int>(y + 1, x + 1);
 
-            if (i1 >= 0 && i2 >= 0 && i3 >= 0)
+            bool is_new1 = (vertex_map_new.at<unsigned char>(y, x) == 1);
+            bool is_new2 = (vertex_map_new.at<unsigned char>(y, x) == 1);
+            bool is_new3 = (vertex_map_new.at<unsigned char>(y, x) == 1);
+            bool is_new4 = (vertex_map_new.at<unsigned char>(y, x) == 1);
+
+            if (i1 >= 0 && i2 >= 0 && i3 >= 0 && (is_new1 || is_new2 || is_new3))
                 wm.addTriangle(i2, i1, i3);
 
-            if (i2 >= 0 && i3 >= 0 && i4 >= 0)
+            if (i2 >= 0 && i3 >= 0 && i4 >= 0 && (is_new2 || is_new3 || is_new4))
                 wm.addTriangle(i2, i3, i4);
         }
     }
@@ -68,12 +132,14 @@ int main(int argc, char **argv)
 
     mwm::Viewer viewer("world model", 640, 480);
 
+    mwm::WorldModel wm;
+
     while(ros::ok())
     {
         geo::Pose3D sensor_pose;
         rgbd::ImageConstPtr image;
 
-        if (!image_buffer.waitForRecentImage("/amigo/base_link", image, sensor_pose, 1.0))
+        if (!image_buffer.waitForRecentImage("/map", image, sensor_pose, 1.0))
             continue;
 
         const cv::Mat& depth_image = image->getDepthImage();
@@ -87,8 +153,6 @@ int main(int argc, char **argv)
         P.setOpticalTranslation(cam_model.getOpticalTranslationX(), cam_model.getOpticalTranslationY());
 
         std::cout << "-----------------" << std::endl;
-
-        mwm::WorldModel wm;
 
         Timer timer;
         update(wm, depth_image, sensor_pose, P);
